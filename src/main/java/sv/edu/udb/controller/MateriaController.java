f63595f4ba2a96af0df49669c0d8a491f2218f26
xGiovax <giovanni.ruano.2006@gmail.com>
2026-04-18 09:26:31 -0600

    agregar: Controlador de Materia

---
+++ src/main/java/sv/edu/udb/controller/MateriaController.java
@@ -0,0 +1,31 @@
+package sv.edu.udb.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import sv.edu.udb.domain.Materia;
+import sv.edu.udb.service.MateriaService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/materias")
+@Tag(name = "Materias", description = "Operaciones para la gestión de asignaturas")
+public class MateriaController {
+
+    @Autowired
+    private MateriaService materiaService;
+
+    @Operation(summary = "Listar todas las materias")
+    @GetMapping
+    public List<Materia> getAll() {
+        return materiaService.findAll();
+    }
+
+    @Operation(summary = "Registrar una nueva materia")
+    @PostMapping
+    public Materia create(@RequestBody Materia materia) {
+        return materiaService.save(materia);
+    }
+}
