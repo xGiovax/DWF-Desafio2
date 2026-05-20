d6fb6092bb311ab9bdbb559bd78e33de75d384c1
xGiovax <giovanni.ruano.2006@gmail.com>
2026-05-20 17:55:58 -0600

    agregar: clase JwtFilter

---
+++ src/main/java/sv/edu/udb/security/UserDetailsServiceImpl.java
@@ -0,0 +1,30 @@
+package sv.edu.udb.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import sv.edu.udb.repository.UsuarioRepository;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        sv.edu.udb.domain.Usuario usuario = usuarioRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado: " + username));
+
+        return new User(
+                usuario.getUsername(),
+                usuario.getPassword(),
+                List.of(new SimpleGrantedAuthority(usuario.getRol()))
+        );
+    }
+}
