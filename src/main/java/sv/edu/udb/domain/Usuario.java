c7b8046b735f14515a6b56c7d71610a6812d1c8a
xGiovax <giovanni.ruano.2006@gmail.com>
2026-05-20 17:31:19 -0600

    agregar: clase Usuario

---
+++ src/main/java/sv/edu/udb/domain/Usuario.java
@@ -0,0 +1,23 @@
+package sv.edu.udb.domain;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "usuario")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String rol; // Ej: "ROLE_USER" o "ROLE_ADMIN"
+}
